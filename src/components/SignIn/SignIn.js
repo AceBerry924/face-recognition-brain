e82552539e7524bf462ff21c64054a7be060e585
LeoPrince <meobius123@gmail.com>
2020-02-26 16:32:08 +0800

    deply

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitLogin = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://salty-shelf-26991.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
